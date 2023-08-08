7c36d3cec0ba1a4c2de93a01f88e26602455f4a3
Ankit Patel <ankit288ap@gmail.com>
2023-08-08 21:40:09 +0530

    Initial commit

---
+++ Start.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.sql.Date;
+
+/**
+ * This class contains the main method .
+ * It calls a function called run() which is used to the entire program
+ */
+public class Start{
+	public static void main(String[] args)
+	{
+		WeCareRentSystem rentSystem =new WeCareRentSystem();
+		rentSystem.run();
+	}
+}
+
+
+
+
+
+
+
